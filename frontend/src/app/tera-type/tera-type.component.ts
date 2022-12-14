65111bb258810d27c94e8498e4a2e7a978599769
Gabrielle <chicklightning@users.noreply.github.com>
2022-12-13 23:11:07 -0800

    adding tera type selector

--- frontend/src/app/tera-type/tera-type.component.ts
+++ frontend/src/app/tera-type/tera-type.component.ts
@@ -1,10 +1,29 @@
 import { Component } from '@angular/core';
 
+import { PokemonType } from '../services/pokemontype';
+import { PokemonTypeService } from '../services/pokemontype.service';
+import { MessageService } from '../services/message.service';
+
 @Component({
   selector: 'app-tera-type',
   templateUrl: './tera-type.component.html',
   styleUrls: ['./tera-type.component.css']
 })
 export class TeraTypeComponent {
+  teraTypes: PokemonType[];
+  selectedType: PokemonType;
 
+  constructor(private pokemonTypeService: PokemonTypeService, private messageService: MessageService) { }
+
+  ngOnInit(): void {
+    this.getPokemonTypes();
+  }
+
+  onSelect(selectedType: PokemonType): void {
+    this.selectedType = selectedType;
+  }
+
+  getPokemonTypes(): void {
+    this.teraTypes = this.pokemonTypeService.getPokemonTypes();
+  }
 }
