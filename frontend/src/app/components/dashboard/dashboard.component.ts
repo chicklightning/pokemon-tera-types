ad8bb307aa61750275295ed66f1f16d542b8b834
Gabrielle <chicklightning@users.noreply.github.com>
2022-12-15 18:58:37 -0800

    some usability fixes >:|

--- frontend/src/app/components/dashboard/dashboard.component.ts
+++ frontend/src/app/components/dashboard/dashboard.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, OnInit, Renderer2 } from '@angular/core';
 import { Pokemon } from 'src/app/services/pokemon';
 import { PokemonType } from 'src/app/services/pokemontype';
 
@@ -11,16 +11,26 @@ export class DashboardComponent implements OnInit {
   selectedTeraType: PokemonType;
   selectedRaidPokemon: Pokemon;
 
-  constructor() { }
+  constructor(private renderer: Renderer2) { }
 
   ngOnInit(): void {
   }
 
   onSelectedTeraType(selectedType: PokemonType): void {
     this.selectedTeraType = selectedType;
+    document.getElementById("raid").scrollIntoView({
+      behavior: "smooth",
+      block: "start",
+      inline: "nearest"
+    });
   }
 
   onSelectedRaidPokemon(selectedPokemon: Pokemon): void {
     this.selectedRaidPokemon = selectedPokemon;
+    document.getElementById("suggestions").scrollIntoView({
+      behavior: "smooth",
+      block: "start",
+      inline: "nearest"
+    });
   }
 }
